7bc175c8bcf8539e3802ff9f875049dd83b0bb8e
Kingston robert <girijarobert1977@gmail.com>
2026-03-10 01:24:04 +0530

    check with ar 1

--- Assets/script code/ModeSwticher.cs
+++ Assets/script code/ModeSwticher.cs
@@ -1,20 +1,37 @@
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
-public class ModeSwitcher : MonoBehaviour {
-    public GameObject simGroup; // Drag Simulation_Group here
-    public GameObject arGroup;  // Drag AR_Group here
-    public ARSession arSession; // Drag AR Session here
+public class ModeSwitcher : MonoBehaviour 
+{
+    public GameController gameController; // Drag the object with GameController script here
+    public GameObject simulationGroup; 
+    public GameObject arGroup;         
+    public ARSession arSession;      
+    
+    public Camera simCamera; // Drag your 3D Simulation Camera here
+    public Camera arCamera;  // Drag the Camera inside XR Origin here
 
-    public void SwitchToAR() {
-        simGroup.SetActive(false);
+    public void SwitchToAR() 
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+        
+        simulationGroup.SetActive(false);
         arGroup.SetActive(true);
-        arSession.enabled = true; // Turns on phone camera
+        arSession.enabled = true; 
+
+        // Update the GameController to use the AR Camera
+        if (gameController != null) gameController.mainCam = arCamera;
     }
 
-    public void SwitchToSim() {
+    public void SwitchToSim() 
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+
         arGroup.SetActive(false);
-        simGroup.SetActive(true);
-        arSession.enabled = false; // Turns off phone camera to save battery
+        simulationGroup.SetActive(true);
+        arSession.enabled = false; 
+
+        // Update the GameController to use the Simulation Camera
+        if (gameController != null) gameController.mainCam = simCamera;
     }
 }
